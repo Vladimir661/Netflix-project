08ff97011283390f77629e66a4d50f029c4775f5
Vladimir661 <xphigota@gmail.com>
2024-11-20 15:27:14 +0200

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './components/style/mainStyle.css';
-import Header from './components/pages/Header';
-import Content from './components/pages/Content';
+import Header from './components/Header';
+import Content from './components/Content';
 import Profile from './components/pages/Profile';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import '../style/swiperStyle.css';
+import './style/swiperStyle.css';
 
 function Slider() {
   const swiperRef = useRef(null);
